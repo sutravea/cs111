9d8f02f656741ed6235f0fec6152aa07cc5de3bb
Abhishek Sutrave <sutrave.abhishek@gmail.com>
2016-02-08 17:22:07 -0500

    INITIALCOMMIT

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld{
+    public static void main(String[]args){
+	System.out.println("hello world!");
+	IO.outputStringAnswer("hello world!");
+	int a = 2;
+	a = a+2;
+
+	int b = 3;
+	b = 4;
+    }
+}
+
---
+++ hw1/Sum.java
@@ -0,0 +1,21 @@
+public class Sum{
+
+    private static int queryInput(){
+	return IO.readInt();
+    }
+
+    private static int add(int n1,int n2){
+	return (n1+n2);
+    }
+
+    private static void dispOutput(int output){
+	IO.outputIntAnswer(output);
+    }
+    public static void main(String[]args){
+	int num1,num2,ans;
+	num1 = queryInput();
+	num2 = queryInput();
+	ans = add(num1,num2);
+	dispOutput(ans);
+    }
+}
