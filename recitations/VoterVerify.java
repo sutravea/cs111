c57dc819ac2fb79abe58facdc0b6c4862a163cc9
Abhishek Sutrave <sutrave.abhishek@gmail.com>
2016-02-08 20:50:26 -0500

    recitation initial commit

---
+++ recitations/VoterVerify.java
@@ -0,0 +1,13 @@
+public class VoterVerify{
+    public static void main(String[]args){
+	IO.outputStringAnswer("What is your name?");
+	String name = IO.readString();
+	IO.outputStringAnswer("What is your age, "+name+"?");
+	int age = IO.readInt();
+	if(age>=18)
+	    IO.outputStringAnswer("You can vote "+name+"!");
+	else
+	    IO.outputStringAnswer("You cannot vote "+name+".");
+    }
+}
+	
